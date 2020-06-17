5b0e8218f5210e4cedeb25858514359641d52593
liangchengj <liangchengj@outlook.com>
2020-06-17 14:16:32 +0800

    First version :: 2020.6.17.1332

--- app/src/main/java/com/meyoustu/aconst/app/MainActivity.java
+++ app/src/main/java/com/meyoustu/aconst/app/MainActivity.java
@@ -1,8 +1,8 @@
 package com.meyoustu.aconst.app;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
 import com.meyoustu.aconst.SharedPrefsConstants;
 
 public class MainActivity extends AppCompatActivity {
